548653a11a7ec9ad570f31ea600a3f87042fc495
d00242634 <d00242634@student.dkit.ie>
2021-11-17 12:03:51 +0000

    Added Rectangle

--- src/main/java/com/dkit/oop/Rectangle.java
+++ src/main/java/com/dkit/oop/Rectangle.java
@@ -2,11 +2,43 @@ package com.dkit.oop;
 //TODO
 // change Rectangle declaration so that Rectangle inherits from Shape
 
-public class Rectangle {
+public class Rectangle extends Shape{
 
+    private int width,length;
+
+    public Rectangle(int x, int y, int width, int length) {
+        super(x, y); //calls the superclass constructor
+
+        this.length=length;
+        this.width=width;
+    }
     //TODO
     // Rectangle requires fields 'width' and 'height'.
     // implement toString() and getters/setters.
     // area() must be implemented.
 
+    @Override
+    public double area(){
+        return this.width*this.length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        // call superclass toString method to get Shape details
+        return super.toString() + "[Width=" + this.width + "]\n[Length=" + this.length + "]";
+    }
 }
